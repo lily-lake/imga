bceff78c39b9fcdcdb35ec6fbd82cb9bf239408d
Lily Lake <robinlake@Lilypad.local>
2025-11-28 17:40:16 -0800

    generate and store short codes, return in response

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -16,25 +17,50 @@ type ShortenURLParams struct {
 
 // Shorten URL response
 type ShortenURLResponse struct {
-	ShortCode string
-	ShortURL string
+	ShortCode   string
+	ShortURL    string
 	OriginalURL string
 }
 
 type Error struct {
-	Code int
+	Code    int
 	Message string
 }
+
+// URL short code map
+var (
+	URLMap = make(map[string]string)
+)
 
 // URL validator
 func isValidURL(rawURL string) bool {
 	_, err := url.ParseRequestURI(rawURL)
-	if err != nil {
-		return false
-	}
-	return true
+	return err == nil
 }
 
+// Generate short code
+func generateShortCode() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, 6)
+	for i := range b {
+		// Generate random byte
+		randomByte := make([]byte, 1)
+		rand.Read(randomByte)
+		b[i] = charset[randomByte[0]%byte(len(charset))]
+	}
+	return string(b)
+}
+
+// Generate code that is not already in the map
+func getUniqueShortCode() string {
+	for {
+		code := generateShortCode()
+		if _, exists := URLMap[code]; !exists {
+			return code
+		}
+		// If code exists, try again
+	}
+}
 
 func CreateShortURLHandler(w http.ResponseWriter, r *http.Request) {
 	// Only allow POST requests
@@ -64,7 +90,19 @@ func CreateShortURLHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Generate unique short code
+	shortCode := getUniqueShortCode()
+	URLMap[shortCode] = params.URL
+
+	// Create response
+	response := ShortenURLResponse{
+		ShortCode:   shortCode,
+		ShortURL:    fmt.Sprintf("http://localhost:8080/%s", shortCode),
+		OriginalURL: params.URL,
+	}
+
 	log.Printf("shorten request for URL: %s", params.URL)
 
-	fmt.Fprintf(w, "create short url route\n")
+	// Encode response as JSON
+	json.NewEncoder(w).Encode(response)
 }
